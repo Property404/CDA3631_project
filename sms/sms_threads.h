f76c50bbc06fe08c58d1f49238b4e1b50a2f8e4c
Property404 <daganboy@gmail.com>
2017-11-30 22:15:58 -0500

    Adding to remove

--- sms/sms_threads.h
+++ sms/sms_threads.h
@@ -1,5 +1,7 @@
 #ifndef _HEADER_SMS_THREADS
 #define _HEADER_SMS_THREADS
 #include "rtx_os.h"
-//void thdAddTextMessage (void *argument);
+int Init_thdAddTextMessage (void);
+int Init_thdDisplayMessages (void);
+osThreadId_t tid_thdDisplayMessages;
 #endif
