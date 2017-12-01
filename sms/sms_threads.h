b4f364d309de697c8dceeaad0ea1a7c9631c4421
Property404 <daganboy@gmail.com>
2017-11-30 22:16:06 -0500

    Removed sms_threads

--- sms/sms_threads.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HEADER_SMS_THREADS
-#define _HEADER_SMS_THREADS
-#include "rtx_os.h"
-int Init_thdAddTextMessage (void);
-int Init_thdDisplayMessages (void);
-osThreadId_t tid_thdDisplayMessages;
-#endif
